40924ba9a6413857828df2277b18c084c9aad434
zyzhang <lzq@123456>
2024-11-29 15:29:41 +0800

    update

--- sdkjs-plugins/content/ftquestion/scripts/question.js
+++ sdkjs-plugins/content/ftquestion/scripts/question.js
@@ -1,38 +1,37 @@
 (function(window, undefined){
 	window.Asc.plugin.init = function init() {
-        this.callCommand(() => {
-          const oDocument = Api.GetDocument()
-          const oParagraph = Api.CreateParagraph()
-          oParagraph.AddText("Hello world!")
-          oDocument.InsertContent([oParagraph])
-        }, true)
+        // this.callCommand(() => {
+        //   const oDocument = Api.GetDocument()
+        //   const oParagraph = Api.CreateParagraph()
+        //   oParagraph.AddText("Hello world!")
+        //   oDocument.InsertContent([oParagraph])
+        // }, true)
 
-        this.executeMethod("AddToolbarMenuItem", [getToolbarItems()]);
+        // this.executeMethod("AddToolbarMenuItem", [getToolbarItems()]);
 
     }
     window.Asc.plugin.button = function button(id) {
-      console.log("按钮事件", id)
     }
-    function getToolbarItems() {
-      let items = {
-        guid: window.Asc.plugin.info.guid,
-        tabs: [{
-          id: "tab_1",
-          text: "Insert options",
-          items: [
-            {
-              id: "insertQuestion",
-              type: "button",
-              text: "Insert Question",
-              hint: "insert question into the document",
-              icons: "resources/buttons/icon.png", 
-              lockInViewMode: true,
-              enableToggle: false,
-              separator: false
-            },
-          ]
-        }]
-      };
-      return items
-    }
+    // function getToolbarItems() {
+    //   let items = {
+    //     guid: window.Asc.plugin.info.guid,
+    //     tabs: [{
+    //       id: "tab_1",
+    //       text: "Insert options",
+    //       items: [
+    //         {
+    //           id: "insertQuestion",
+    //           type: "button",
+    //           text: "Insert Question",
+    //           hint: "insert question into the document",
+    //           icons: "resources/buttons/icon.png", 
+    //           lockInViewMode: true,
+    //           enableToggle: false,
+    //           separator: false
+    //         },
+    //       ]
+    //     }]
+    //   };
+    //   return items
+    // }
 })(window, undefined);
