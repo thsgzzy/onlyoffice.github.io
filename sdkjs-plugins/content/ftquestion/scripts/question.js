2ff141c0eb8eeac72038dcf243105ef8c7799753
zyzhang <lzq@123456>
2024-11-29 10:17:44 +0800

    引入jquery

--- sdkjs-plugins/content/ftquestion/scripts/question.js
+++ sdkjs-plugins/content/ftquestion/scripts/question.js
@@ -7,32 +7,32 @@
           oDocument.InsertContent([oParagraph])
         }, true)
 
-        // this.executeMethod("AddToolbarMenuItem", [getToolbarItems()]);
+        this.executeMethod("AddToolbarMenuItem", [getToolbarItems()]);
 
     }
     window.Asc.plugin.button = function button(id) {
       console.log("按钮事件", id)
     }
-    // function getToolbarItems() {
-    //   let items = {
-    //     guid: window.Asc.plugin.info.guid,
-    //     tabs: [{
-    //       id: "tab_1",
-    //       text: "Insert options",
-    //       items: [
-    //         {
-    //           id: "insertQuestion",
-    //           type: "button",
-    //           text: "Insert Question",
-    //           hint: "insert question into the document",
-    //           icons: "resources/buttons/icon.png", 
-    //           lockInViewMode: true,
-    //           enableToggle: false,
-    //           separator: false
-    //         },
-    //       ]
-    //     }]
-    //   };
-    //   return items
-    // }
+    function getToolbarItems() {
+      let items = {
+        guid: window.Asc.plugin.info.guid,
+        tabs: [{
+          id: "tab_1",
+          text: "Insert options",
+          items: [
+            {
+              id: "insertQuestion",
+              type: "button",
+              text: "Insert Question",
+              hint: "insert question into the document",
+              icons: "resources/buttons/icon.png", 
+              lockInViewMode: true,
+              enableToggle: false,
+              separator: false
+            },
+          ]
+        }]
+      };
+      return items
+    }
 })(window, undefined);
