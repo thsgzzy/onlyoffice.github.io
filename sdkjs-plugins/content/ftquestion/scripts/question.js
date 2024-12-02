7665c106963d299e16756a359334d0c4dde99a58
zyzhang <lzq@123456>
2024-12-02 09:45:27 +0800

    将项目添加到工具栏菜单

--- sdkjs-plugins/content/ftquestion/scripts/question.js
+++ sdkjs-plugins/content/ftquestion/scripts/question.js
@@ -12,6 +12,36 @@
     }
     window.Asc.plugin.button = function button(id) {
     }
+
+    var oToolbarMenuItem = {
+        "id": "FtMenuItem",
+        "type": "button",
+        "text": "FT",
+        "hint": "FT",
+        "icons": "resources/icon.png",
+        "disabled": false,
+        "enableToggle": false,
+        "lockInViewMode": false,
+        "separator": true,
+        "split": true,
+        "items": [
+            {
+                "id": "onMenuFt",
+                "text": "Explain text in comment"
+            },
+        ]
+    };
+    var oToolbarMenuTab = {
+        "id": "fangtian",
+        "text": "方田题库",
+        "items": [oToolbarMenuItem]
+    };
+    var oToolbarMenuMainItem = {
+        "guid": "asc.{da92c56c-dd26-4b31-a391-90f7d8c72ecc}",
+        "tabs": [oToolbarMenuTab]
+    };
+    window.Asc.plugin.executeMethod ("AddToolbarMenuItem", [oToolbarMenuMainItem]);
+
     // function getToolbarItems() {
     //   let items = {
     //     guid: window.Asc.plugin.info.guid,
